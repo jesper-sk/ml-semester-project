d39b70c97724702af4fbf32f88c4d7e0b20ac0ce
JesperK123456 <47450353+JesperK123456@users.noreply.github.com>
2020-12-10 14:52:26 +0100

    changed 0 to silence (nan)

--- data/plot_ssm.py
+++ data/plot_ssm.py
@@ -9,12 +9,14 @@ METRIC = 'euclidean'
 f = np.genfromtxt('F.txt')
 
 min_key_nr = np.min(f[np.nonzero(f)])
+f[f==0] = np.nan
 fn = np.maximum(f - min_key_nr, 0) % 12
+
 ds = [squareform(pdist(fn[:,x], metric=METRIC)) for x in 
       product(range(4), range(4))]
 for d in ds:
-    d -= np.min(d)
-    d /= (np.max(d) - np.min(d))
+    d -= np.nanmin(d)
+    d /= (np.nanmax(d) - np.nanmin(d))
 
 for x in product(range(4), range(4)):
     fig, ax = plt.subplots(figsize=(20,20))
