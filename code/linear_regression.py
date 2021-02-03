f444ebf9d106aa6522c164684d52a74a2ad595dd
Ryan Oloughlin <rmoloughlin11@gmail.com>
2021-02-03 10:35:53 +0100

    First draft of linear regression

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -1,4 +1,21 @@
-
+import numpy as np
 
 def obtain_optimal_weights(X,Y):
-    return []
+
+    W = np.zeros((60,1))
+    steps = len(Y)-len(X)
+    for i in range(steps):
+
+        x = X[i,:].reshape(60,1)
+        y = Y[i+10+1,:].reshape(6,1)
+
+        w = np.dot(np.linalg.pinv(np.dot(x,np.transpose(x))),x)
+
+        for j in range(10):
+            for jj in range(6):
+                w[j+jj] = w[j+jj]*y[jj]
+        W += w
+
+    W = W/steps
+    print(W)
+    return W
--- code/output.py
+++ code/output.py
@@ -1,4 +1,6 @@
 import numpy as np
+from input import raw_to_features
 
-def construct_output(F, indices):
+
+def construct_output(F, indices, voice):
     pass
