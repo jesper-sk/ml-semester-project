3d58b340d069b2ab1be9555c94addf220e1ce219
Leander <33727726+Saltpile123@users.noreply.github.com>
2021-02-09 18:10:51 +0100

    changed midi

--- code/midi.py
+++ code/midi.py
@@ -2,8 +2,15 @@ import numpy as np
 import mido
 from audio import save_inferences_to_midi
 from linear_regression import Inferences
+from transform import encode_duration
 
 
 if __name__ == "__main__":
     raw_input = np.genfromtxt('../data/F.txt', dtype=int)
-    
+    infs = []
+    for v in range(4):
+        voice = []
+        for note, duration in encode_duration(raw_input, v):
+            voice.append(Inference((note, duration)))
+        infs.append(voice)
+        
